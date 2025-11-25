7796ecc88d6cf8e52d98f145c7b3cfabcdebc389
khademul <milon0@gmail.com>
2025-11-25 19:15:53 +0600

    commit

---
+++ Wallet_dxchange.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t,a,b,c;
+    cin>>t;
+    while(t--){
+        cin>>a>>b;
+        if((a+b)%2==0)
+        cout<<"Bob"<<endl;
+        else cout<<"Alice"<<endl;
+    }
+}
