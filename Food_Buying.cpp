fb319654c4935378506beabbdda9cdeff42a4e90
khademul <milon0@gmail.com>
2025-12-04 23:38:05 +0600

    tmi bojla na

---
+++ Food_Buying.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long s, ans = 0;
+        cin >> s;
+        while(s >= 10){
+            ans += (s/10) * 10;
+            s = (s/10) + (s%10);
+        }
+        ans += s;
+        cout << ans << endl;
+    }
+}
