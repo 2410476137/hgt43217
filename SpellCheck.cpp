7856eefe8cdb003eca14ae4df0a02fe4882552e0
khademul <milon0@gmail.com>
2025-12-20 21:49:47 +0600

    ami to boji na to son

---
+++ SpellCheck.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t,i,j,n;
+  cin>>t;
+  while(t--){
+    cin>>n;
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+   // cout<<s<<endl;
+    if(s=="Timru")
+    cout<<"YES"<<endl;
+    else 
+    cout<<"NO"<<endl;
+
+  }  
+}
